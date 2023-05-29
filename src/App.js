c1491633bcb391a3a4442431f464171afed41993
naanmohammed <naanahmed24@gmail.com>
2023-05-29 20:42:38 +0300

    Add nav bar structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from './components/Nav/navBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit 
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+      </Router>
     </div>
   );
 }
--- src/components/Nav/navBar.jsx
+++ src/components/Nav/navBar.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-// import HomeList from './HomeList';
+import { Link } from 'react-router-dom';
+import './navBar.css';
+import mic from '../../Assets/mic.png';
+import setting from '../../Assets/setting.png';
+import lessthan from '../../Assets/less-than.png';
 
-function HomeContainer() {
+function Navigation() {
   return (
-    <div className="home">
-      {/* <HomeList /> */}
-      <p>List comming soon</p>
-    </div>
+    <nav>
+      <ul className="navItems">
+        <li>
+          <img src={lessthan} alt="less than Icon" className="lessThan" />
+          <Link to="/" className="home">Home</Link>
+        </li>
+        <li>
+          <h1>Crypto Info</h1>
+        </li>
+        <li>
+          <div className="icons">
+            <img src={mic} alt="Mic Icon" />
+            <img src={setting} alt="Setting Icon" />
+          </div>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
-export default HomeContainer;
+export default Navigation;
