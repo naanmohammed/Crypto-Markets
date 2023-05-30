424ef6e3f11f71dee0b6dd92609fbfcb6c8ee2d2
naanmohammed <naanahmed24@gmail.com>
2023-05-30 12:04:20 +0300

    Update details container

--- src/components/Description/DetailsContainer.jsx
+++ src/components/Description/DetailsContainer.jsx
@@ -20,123 +20,115 @@ function DetailsContainer() {
   const filteredCrypto = cryptos.filter((crypto) => crypto.show === true);
 
   return (
-      <div className="detailsContainer">
-        {
-          filteredCrypto.map((crypto) => (
-            <>
-              <div key={uuidv4()} className="imageContainer">
-                <img src={crypto.img} alt="coin logo" id={uuidv4} className="indiImg" />
-                <div className="cont">
-                  <h2>
-                    Coin:
-                    {' '}
-                    {crypto.name}
-                  </h2>
-                  <h2>
-                    Ranked at:
-                    {' '}
-                    #
-                    {crypto.rank}
-                  </h2>
-                </div>
-              </div>
-              <h2 className="detailTitle">
-                More Information about
-                {' '}
-                {crypto.name}
-              </h2>
-              <div className="info">
-                <p>Coin Name</p>
-                <p>{crypto.name}</p>
-              </div>
-              <div className="info">
-                <p>Ranking</p>
-                <p>
-                  No.
+    <div className="detailsContainer">
+      {
+        filteredCrypto.map((crypto) => (
+          <>
+            <div key={uuidv4()} className="imageContainer">
+              <img src={crypto.img} alt="coin logo" id={uuidv4} className="indiImg" />
+              <div className="cont">
+                <h2>
+                  Coin:
+                  {' '}
+                  {crypto.name}
+                </h2>
+                <h2>
+                  Ranked at:
+                  {' '}
+                  #
                   {crypto.rank}
-                </p>
+                </h2>
               </div>
-              <div className="info">
-                <p>Current Price</p>
-                <p>
-                  $
-                  {crypto.price}
-                </p>
-              </div>
-              <div className="info">
-                <p>Symbol</p>
-                <p>{crypto.symbol}</p>
-              </div>
-              <div className="info">
-                <p>Market Cap</p>
-                <p>
-                  $
-                  {crypto.marketCap}
-                </p>
-              </div>
-              <div className="info">
-                <p>Available Supply</p>
-                <p>{crypto.availableSupply}</p>
-              </div>
-              <div className="info">
-                <p>Contract Address</p>
-                <p>{crypto.contractAddress}</p>
-              </div>
-              <div className="info">
-                <p>Decimals</p>
-                <p>{crypto.decimals}</p>
-              </div>
-              <div className="info">
-                <p>Price Btc</p>
-                <p>
-                  $
-                  {crypto.priceBtc}
-                </p>
-              </div>
-              <div className="info">
-                <p>Price Change/1 Day Ago</p>
-                <p>
-                  $
-                  {crypto.priceChange1d}
-                </p>
-              </div>
-              <div className="info">
-                <p>Price Change/Hour</p>
-                <p>
-                  $
-                  {crypto.priceChange1h}
-                </p>
-              </div>
-              <div className="info">
-                <p>Price Change/Week</p>
-                <p>
-                  $
-                  {crypto.priceChange1w}
-                </p>
-              </div>
-              <div className="info">
-                <p>Total Supply</p>
-                <p>
-                  $
-                  {crypto.totalSupply}
-                </p>
-              </div>
-              <div className="info">
-                <p>Volume</p>
-                <p>{crypto.volume}</p>
-              </div>
-              <div className="infoLink">
-                <p>Twitter</p>
-                <li><a href={crypto.twitterUrl}>Our Twitter Handle</a></li>
-              </div>
-              <div className="infoLink">
-                <p>Website</p>
-                <li><a href={crypto.websiteUrl}>Our website URL</a></li>
-              </div>
-            </>
-          ))
-        }
-      </div>
+            </div>
+            <h2 className="detailTitle">
+              More Information about
+              {' '}
+              {crypto.name}
+            </h2>
+            <div className="info">
+              <p>Coin Name</p>
+              <p>{crypto.name}</p>
+            </div>
+            <div className="info">
+              <p>Ranking</p>
+              <p>
+                No.
+                {crypto.rank}
+              </p>
+            </div>
+            <div className="info">
+              <p>Current Price</p>
+              <p>
+                $
+                {crypto.price}
+              </p>
+            </div>
+            <div className="info">
+              <p>Symbol</p>
+              <p>{crypto.symbol}</p>
+            </div>
+            <div className="info">
+              <p>Market Cap</p>
+              <p>
+                $
+                {crypto.marketCap}
+              </p>
+            </div>
+            <div className="info">
+              <p>Available Supply</p>
+              <p>{crypto.availableSupply}</p>
+            </div>
+            <div className="info">
+              <p>Contract Address</p>
+              <p>{crypto.contractAddress}</p>
+            </div>
+            <div className="info">
+              <p>Decimals</p>
+              <p>{crypto.decimals}</p>
+            </div>
+            <div className="info">
+              <p>Price Btc</p>
+              <p>
+                $
+                {crypto.priceBtc}
+              </p>
+            </div>
+            <div className="info">
+              <p>Price Change/1 Day Ago</p>
+              <p>
+                $
+                {crypto.priceChange1d}
+              </p>
+            </div>
+            <div className="info">
+              <p>Price Change/Hour</p>
+              <p>
+                $
+                {crypto.priceChange1h}
+              </p>
+            </div>
+            <div className="info">
+              <p>Price Change/Week</p>
+              <p>
+                $
+                {crypto.priceChange1w}
+              </p>
+            </div>
+            <div className="info">
+              <p>Total Supply</p>
+              <p>
+                $
+                {crypto.totalSupply}
+              </p>
+            </div>
+            <div className="info">
+              <p>Volume</p>
+              <p>{crypto.volume}</p>
+            </div>
+          </>
+        ))
+      }
+    </div>
   );
 }
 export default DetailsContainer;
