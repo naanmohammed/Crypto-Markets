89607645ec9a5ea4d8350c9f920bc0a5473da763
naanmohammed <naanahmed24@gmail.com>
2023-05-30 11:47:48 +0300

    Add home list

---
+++ src/components/Home/homeList.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import HomeItem from './homeItem';
+import { fetchCoins } from '../../Redux/cryptoSlice';
+
+function HomeList() {
+  const [query, setQuery] = useState('');
+  // eslint-disable-next-line
+  function handleSearch(event) {
+    setQuery(event.target.value);
+  }
+
+  const cryptos = useSelector((state) => state.crypto);
+
+  const filteredCrypto = cryptos.filter((crypto) => (
+    crypto.name.toLowerCase().includes(query.toLowerCase())
+  ));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
+
+  return (
+    <ul className="itemList">
+      {filteredCrypto.map((crypto) => 
+        <HomeItem key={crypto.id} crypto={crypto} />)}
+    </ul>
+  );
+}
+
+export default HomeList;
