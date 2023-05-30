bc9cc62704ce3e075a06b59569939764976e05e3
naanmohammed <naanahmed24@gmail.com>
2023-05-30 13:55:16 +0300

    Add tests for navBar component

---
+++ src/__tests__/navBar.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from '../components/Nav/navBar';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Navigation', () => {
+  it('renders correctly', () => {
+    render(
+      <Router>
+        <Navigation />
+      </Router>
+    );
+
+    expect(screen.getByAltText('less than Icon')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Crypto Info')).toBeInTheDocument();
+    expect(screen.getByAltText('Mic Icon')).toBeInTheDocument();
+    expect(screen.getByAltText('Setting Icon')).toBeInTheDocument();
+  });
+
+});
